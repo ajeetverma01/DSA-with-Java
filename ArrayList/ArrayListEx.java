28002c63684e49d3d70b5184ab3c0c5a9917c0c9
Ajeet Verma <av401402@gmail.com>
2025-10-18 05:22:36 +0530

    ArrayList package created, and a basic example

---
+++ ArrayList/ArrayListEx.java
@@ -0,0 +1,20 @@
+
+import java.util.ArrayList;
+
+public class ArraylistEX {
+    static void arrayList() {
+        ArrayList<Integer> ar = new ArrayList<Integer>(3); //It can increase its capacity...
+        ar.add(20);
+        ar.add(25);
+        ar.add(22);
+        ar.add(25);
+        ar.add(22);
+
+        ar.set(2, 56); //To update
+        System.out.println(ar);
+    }
+
+    public static void main(String[] args) {
+        arrayList();
+    }
+}
