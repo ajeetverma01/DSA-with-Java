6d972e78b56e7b544bd6b327409915f0b21cfa3a
Ajeet Verma <av401402@gmail.com>
2025-10-17 21:30:44 +0530

    Two Dimensional array

---
+++ Arrays/TwoDArray.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TwoDArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[3][3];
+        System.out.println(Arrays.toString(arr[0]));
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("\nWay 1");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nWay 2...");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }
+
+        System.out.println("\nWay 3");
+        for (int[] is : arr) {
+            System.out.println(Arrays.toString(is));
+        }
+        sc.close();
+    }
+
+}
