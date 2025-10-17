37a4cb133fabecc2cf193716f7a4c6d4e244b9b8
Ajeet Verma <av401402@gmail.com>
2025-10-17 21:48:58 +0530

    Multi .dimentional array learned

---
+++ Arrays/MultiDimentionalArray.java
@@ -0,0 +1,31 @@
+package Arrays;
+import java.util.Scanner;
+
+public class MultiDimentionalArray {
+    public static void main(String[] args) {
+        String arr[][][] = new String[3][3][3];
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input in 3d array
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                for (int j2 = 0; j2 < arr[i][j].length; j2++) {
+                    arr[i][j][j2] = sc.nextLine();
+                }
+            }
+        }
+
+        System.err.println("Printing.............................");
+        // Printing 3d array
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                for (int j2 = 0; j2 < arr[i][j].length; j2++) {
+                    System.out.print(arr[i][j][j2]);
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
