0774d4a542ee2f1247b6fbe4e655965069dfbfcc
Ajeet Verma <av401402@gmail.com>
2025-10-18 05:29:53 +0530

    ex Arraylist

--- ArrayList/ArrayListEx.java
+++ ArrayList/ArrayListEx.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 public class ArraylistEX {
     static void arrayList() {
+        // We pass wrapper classes inside AL
         ArrayList<Integer> ar = new ArrayList<Integer>(3); //It can increase its capacity...
         ar.add(20);
         ar.add(25);
