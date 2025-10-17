4bff06189ca6cad8313e8a183be79d5f571fca76
Ajeet Verma <av401402@gmail.com>
2025-10-17 21:31:11 +0530

    Two Dimensional array

---
+++ Arrays/TwoDArray2.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+// The number of column is not fixed
+public class TwoDArray2 {
+    public static void main(String[] args) {
+        int [][]arr = {
+            {1,2,3,4},
+            {5,6},
+            {7,8,9}
+        };
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        // Alternative
+        for (int[] is : arr) {
+            System.out.println(Arrays.toString(is));
+        }
+    }
+}
